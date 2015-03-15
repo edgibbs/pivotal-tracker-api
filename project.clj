97cc467ae8a9ea6380172a9b51ac3ea6c09acda2
Ed Gibbs <edward_gibbs@yahoo.com>
2015-03-15 08:45:58 -0700

    Switch to MIT license

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject pivotal-tracker-api "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]
                                   [clj-http-fake "1.0.1"]]
-                    :plugins [[lein-midje "3.1.3"]]}})
+                   :plugins [[lein-midje "3.1.3"]]}})
